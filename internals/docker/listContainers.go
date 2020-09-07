a7ebfa16fee23b609622a42810f89636dca4a8d8
sharma1612harshit <sharma1612harshit@gmail.com>
2020-09-07 22:25:32 +0530

    docker container driver

--- internals/docker/listContainers.go
+++ internals/docker/listContainers.go
@@ -3,6 +3,7 @@ package docker
 import (
 	"context"
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
 )
 
 func ListContainers() ([]types.Container, error) {
@@ -14,7 +15,16 @@ func ListContainers() ([]types.Container, error) {
 		return nil, err
 	}
 
-	list, err := cli.ContainerList(ctx, types.ContainerListOptions{})
+	list, err := cli.ContainerList(ctx, types.ContainerListOptions{
+		Quiet:   false,
+		Size:    false,
+		All:     false,
+		Latest:  false,
+		Since:   "",
+		Before:  "",
+		Limit:   0,
+		Filters: filters.Args{},
+	})
 
 	return list, err
 }
