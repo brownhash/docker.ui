67618c3f5af37156f64f009a45f3a549d46783ad
sharma1612harshit <sharma1612harshit@gmail.com>
2020-11-23 20:12:51 +0530

    add launch container func

--- pkg/docker/containers.go
+++ pkg/docker/containers.go
@@ -3,7 +3,9 @@ package docker
 import (
 	"context"
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/api/types/network"
 )
 
 // List local docker containers
@@ -28,4 +30,30 @@ func GetContainers(quiet bool, size bool, all bool, latest bool, since string, b
 	})
 
 	return list, err
+}
+
+func LaunchContainer(hostName string, domainName string, user string, imageId string, volumes map[string]struct{}, networkDisabled bool, onBuild []string, labels map[string]string, stopSignal string, stopTimeout *int, tty bool, hostConfig *container.HostConfig, networkingConfig *network.NetworkingConfig, containerName string) (container.ContainerCreateCreatedBody, error) {
+	ctx := context.Background()
+
+	cli, err := Client()
+
+	if err != nil {
+		return container.ContainerCreateCreatedBody{}, err
+	}
+
+	launchResponse, err := cli.ContainerCreate(ctx, &container.Config{
+		Hostname:        hostName,
+		Domainname:      domainName,
+		User:            user,
+		Tty:             tty,
+		Image:           imageId,
+		Volumes:         volumes,
+		NetworkDisabled: networkDisabled,
+		OnBuild:         onBuild,
+		Labels:          labels,
+		StopSignal:      stopSignal,
+		StopTimeout:     stopTimeout,
+	}, hostConfig, networkingConfig, containerName)
+
+	return launchResponse, err
 }
