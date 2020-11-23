a214dcdfa8f7e0231a6f7ba43aaaaff6ed53c6be
sharma1612harshit <sharma1612harshit@gmail.com>
2020-11-23 18:55:41 +0530

    add pull image func

--- pkg/docker/images.go
+++ pkg/docker/images.go
@@ -1,6 +1,7 @@
 package docker
 
 import (
+	"io"
 	"context"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/filters"
@@ -22,6 +23,25 @@ func GetImages(all bool, filters filters.Args) ([]types.ImageSummary, error) {
 	})
 
 	return images, err
+}
+
+// Pull specified docker image
+func PullImage(imageRef string, all bool, registryAuth string) (io.ReadCloser, error) {
+	ctx := context.Background()
+
+	cli, err := Client()
+
+	if err != nil {
+		return nil, err
+	}
+
+	pullResponse, err := cli.ImagePull(ctx, imageRef, types.ImagePullOptions{
+		All:           all,
+		RegistryAuth:  registryAuth,
+		PrivilegeFunc: nil,
+	})
+
+	return pullResponse, err
 }
 
 // Delete specified docker image
