d33653c1315ea969a62f9a0dabd77377834d26dc
sharma1612harshit <sharma1612harshit@gmail.com>
2020-12-29 18:01:40 +0530

    add container config for container create

--- api/docker/containers.go
+++ api/docker/containers.go
@@ -1,6 +1,8 @@
 package docker
 
 import (
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/network"
 	duiTypes "github.com/sharma1612harshit/docker.ui/api/types"
 	"github.com/sharma1612harshit/docker.ui/pkg/docker"
 	"log"
@@ -36,4 +38,81 @@ func GetContainers() ([]duiTypes.ContainerResponse, error) {
 	}
 
 	return containerList, err
+}
+
+// run a container with specified configuration
+func RunContainer(containerName, imageName, hostName, domainName, user string, cmd, entryPoint []string, labels map[string]string) (container.ContainerCreateCreatedBody, error) {
+	containerConf := &container.Config{
+		Hostname:        hostName,
+		Domainname:      domainName,
+		User:            user,
+		Cmd:             cmd,
+		Image:           imageName,
+		Entrypoint:      entryPoint,
+		Labels:          labels,
+		//Env:             "",
+		//Tty:             tty,
+		//AttachStdin:     false,
+		//AttachStdout:    false,
+		//AttachStderr:    false,
+		//ExposedPorts:    "",
+		//NetworkDisabled: false,
+		//MacAddress:      "",
+		//OnBuild:         nil,
+		//Volumes:         "",
+		//WorkingDir:      "",
+		//StopSignal:      "",
+		//StopTimeout:     nil,
+		//Shell:           nil,
+		//OpenStdin:       false,
+		//StdinOnce:       false,
+		//ArgsEscaped:     false,
+	}
+
+	hostConf := &container.HostConfig{
+		Binds:           nil,
+		ContainerIDFile: "",
+		LogConfig:       container.LogConfig{},
+		NetworkMode:     "",
+		PortBindings:    nil,
+		RestartPolicy:   container.RestartPolicy{},
+		AutoRemove:      false,
+		VolumeDriver:    "",
+		VolumesFrom:     nil,
+		CapAdd:          nil,
+		CapDrop:         nil,
+		DNS:             nil,
+		DNSOptions:      nil,
+		DNSSearch:       nil,
+		ExtraHosts:      nil,
+		GroupAdd:        nil,
+		IpcMode:         "",
+		Cgroup:          "",
+		Links:           nil,
+		OomScoreAdj:     0,
+		PidMode:         "",
+		Privileged:      false,
+		PublishAllPorts: false,
+		ReadonlyRootfs:  false,
+		SecurityOpt:     nil,
+		StorageOpt:      nil,
+		Tmpfs:           nil,
+		UTSMode:         "",
+		UsernsMode:      "",
+		ShmSize:         0,
+		Sysctls:         nil,
+		Runtime:         "",
+		ConsoleSize:     [2]uint{},
+		Isolation:       "",
+		Resources:       container.Resources{},
+		Mounts:          nil,
+		Init:            nil,
+		InitPath:        "",
+	}
+
+	netConf := &network.NetworkingConfig{}
+
+	resp, err := docker.LaunchContainer(containerConf, hostConf, netConf, containerName)
+
+	return resp, err
 }
