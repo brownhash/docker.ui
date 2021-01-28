81a7219bbd9cc0f89773fe2202c5f18a2a6857d2
sharma1612harshit <sharma1612harshit@gmail.com>
2021-01-29 02:30:18 +0530

    add logging levels

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,6 +1,7 @@
 package logger
 
 import (
+	"os"
 	"log"
 	"fmt"
 	"net/http"
@@ -14,64 +15,93 @@ const (
 	DebugColor   = "\033[0;36m%s\033[0m"
 )
 
-// Success - log success messages
-func Success(message interface{}) {
-	formatter := fmt.Sprintf(SuccessColor, message)
-	log.Println(formatter)
+// SetLogLevel - set global logging level
+func SetLogLevel(logLevel string) {
+	os.Setenv("DUI_LOGGING_LEVEL", logLevel)
 }
 
-// Successf - log success messages in same line
-func Successf(message interface{}) {
-	formatter := fmt.Sprintf(SuccessColor, message)
-	log.Printf(formatter)
-}
-
-// Info - log informative messages
-func Info(message interface{}) {
-	formatter := fmt.Sprintf(InfoColor, message)
-	log.Println(formatter)
-}
-
-// Infof - log informative messages in same line
-func Infof(message interface{}) {
-	formatter := fmt.Sprintf(InfoColor, message)
-	log.Printf(formatter)
-}
-
-// Warn - log warning messages
-func Warn(message interface{}) {
-	formatter := fmt.Sprintf(WarningColor, message)
-	log.Println(formatter)
-}
-
-// Warnf - log warning messages in same line
-func Warnf(message interface{}) {
-	formatter := fmt.Sprintf(WarningColor, message)
-	log.Printf(formatter)
-}
-
-// Error - log error messages
-func Error(message interface{}) {
-	formatter := fmt.Sprintf(ErrorColor, message)
-	log.Fatal(formatter)
-}
-
-// Errorf - log error messages in same line
-func Errorf(message interface{}) {
-	formatter := fmt.Sprintf(ErrorColor, message)
-	log.Fatalf(formatter)
+func getLogLevel() string {
+	return os.Getenv("DUI_LOGGING_LEVEL")
 }
 
 // Debug - log debugging messages
 func Debug(message interface{}) {
-	formatter := fmt.Sprintf(DebugColor, message)
-	log.Println(formatter)
+	if getLogLevel() == "DEBUG" {
+		formatter := fmt.Sprintf(DebugColor, message)
+		log.Println(formatter)
+	}
 }
 
 // Debugf - log debugging messages in same line
 func Debugf(message interface{}) {
-	formatter := fmt.Sprintf(DebugColor, message)
-	log.Printf(formatter)
+	if getLogLevel() == "DEBUG" {
+		formatter := fmt.Sprintf(DebugColor, message)
+		log.Printf(formatter)
+	}
+}
+
+// Info - log informative messages
+func Info(message interface{}) {
+	if getLogLevel() == "DEBUG" || getLogLevel() == "INFO" {
+		formatter := fmt.Sprintf(InfoColor, message)
+		log.Println(formatter)
+	}
+}
+
+// Infof - log informative messages in same line
+func Infof(message interface{}) {
+	if getLogLevel() == "DEBUG" || getLogLevel() == "INFO" {
+		formatter := fmt.Sprintf(InfoColor, message)
+		log.Printf(formatter)
+	}
+}
+
+// Warn - log warning messages
+func Warn(message interface{}) {
+	if getLogLevel() == "DEBUG" || getLogLevel() == "INFO" || getLogLevel() == "WARN" {
+		formatter := fmt.Sprintf(WarningColor, message)
+		log.Println(formatter)
+	}
+}
+
+// Warnf - log warning messages in same line
+func Warnf(message interface{}) {
+	if getLogLevel() == "DEBUG" || getLogLevel() == "INFO" || getLogLevel() == "WARN" {
+		formatter := fmt.Sprintf(WarningColor, message)
+		log.Printf(formatter)
+	}
+}
+
+// Success - log success messages
+func Success(message interface{}) {
+	if getLogLevel() == "DEBUG" || getLogLevel() == "INFO" || getLogLevel() == "WARN" {
+		formatter := fmt.Sprintf(SuccessColor, message)
+		log.Println(formatter)
+	}
+}
+
+// Successf - log success messages in same line
+func Successf(message interface{}) {
+	if getLogLevel() == "DEBUG" || getLogLevel() == "INFO" || getLogLevel() == "WARN" {
+		formatter := fmt.Sprintf(SuccessColor, message)
+		log.Printf(formatter)
+	}
+}
+
+// Error - log error messages
+func Error(message interface{}) {
+	if getLogLevel() == "DEBUG" || getLogLevel() == "INFO" || getLogLevel() == "WARN" || getLogLevel() == "ERROR" {
+		formatter := fmt.Sprintf(ErrorColor, message)
+		log.Fatal(formatter)
+	}
+}
+
+// Errorf - log error messages in same line
+func Errorf(message interface{}) {
+	if getLogLevel() == "DEBUG" || getLogLevel() == "INFO" || getLogLevel() == "WARN" || getLogLevel() == "ERROR" {
+		formatter := fmt.Sprintf(ErrorColor, message)
+		log.Fatalf(formatter)
+	}
 }
 
 // LogRequest - log http requests
