c6319150e7854ff12c54365b56417a33a1729053
sharma1612harshit <sharma1612harshit@gmail.com>
2021-01-29 01:09:57 +0530

    add export function comments & restrict request methods on images/pullimage api

--- api/handler/handlers.go
+++ api/handler/handlers.go
@@ -8,17 +8,10 @@ import (
 	"net/http"
 )
 
-// handler for images api
+// ImagesHandler - handler for images api
 func ImagesHandler(w http.ResponseWriter, r *http.Request) {
-	images, err := docker.GetImages(r.Header.Get("all"), r.Header.Get("filters"))
-
-	if err != nil {
-		log.Print(err)
-
-		w.WriteHeader(http.StatusInternalServerError)
-		_, _ = w.Write([]byte("500 - Something bad happened!"))
-	} else {
-		imageResponse, err := json.Marshal(images)
+	if r.Method == "GET" {
+		images, err := docker.GetImages(r.Header.Get("all"), r.Header.Get("filters"))
 
 		if err != nil {
 			log.Print(err)
@@ -26,26 +19,43 @@ func ImagesHandler(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusInternalServerError)
 			_, _ = w.Write([]byte("500 - Something bad happened!"))
 		} else {
-			_, _ = w.Write(imageResponse)
+			imageResponse, err := json.Marshal(images)
+
+			if err != nil {
+				log.Print(err)
+
+				w.WriteHeader(http.StatusInternalServerError)
+				_, _ = w.Write([]byte("500 - Something bad happened!"))
+			} else {
+				_, _ = w.Write(imageResponse)
+			}
 		}
-	}
-}
-
-// handler for pull image api
-func PullImageHandler(w http.ResponseWriter, r *http.Request) {
-	status, err := docker.PullImage(r.Header.Get("all"), r.Header.Get("imageref"), r.Header.Get("username"), r.Header.Get("password"))
-
-	if err != nil {
-		log.Print(err)
-
-		w.WriteHeader(http.StatusInternalServerError)
-		_, _ = w.Write([]byte(fmt.Sprintf("%s - %s", status, err)))
 	} else {
-		_, _ = w.Write([]byte(status))
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		_, _ = w.Write([]byte("405 - Method not allowed!"))
 	}
 }
 
-// handler for containers api
+// PullImageHandler - handler for pull image api
+func PullImageHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		status, err := docker.PullImage(r.Header.Get("all"), r.Header.Get("imageref"), r.Header.Get("username"), r.Header.Get("password"))
+
+		if err != nil {
+			log.Print(err)
+
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte(fmt.Sprintf("%s - %s", status, err)))
+		} else {
+			_, _ = w.Write([]byte(status))
+		}
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		_, _ = w.Write([]byte("405 - Method not allowed!"))
+	}
+}
+
+// ContainersHandler - handler for containers api
 func ContainersHandler(w http.ResponseWriter, r *http.Request) {
 	containers, err := docker.GetContainers()
 
@@ -68,6 +78,7 @@ func ContainersHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// LaunchContainerHandler - handler for run container api
 func LaunchContainerHandler(w http.ResponseWriter, r *http.Request) {
 	launchResponse, err := docker.RunContainer(r.Header.Get("name"), r.Header.Get("image_name"), "", "", "", []string{}, []string{}, map[string]string{})
 
